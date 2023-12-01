e45b7f70e385937d9ca5840c51e7d6461d67f068
gitea <gitea@gitea.gitea>
2023-12-01 16:40:46 +0500

    Code review

--- Задание для кандидатов. Выполнить ревью кода.sql
+++ Задание для кандидатов. Выполнить ревью кода.sql
@@ -1,21 +1,36 @@
+/* 
+	1. Название update-скрипта должно соответствовать маске
+	{Number}_{Task}_{Action}[_{AdditionalInfo}].sql, например, "001_TEST-1_ChangeStructure.sql"
+*/
 create procedure syn.usp_ImportFileCustomerSeasonal
+	-- 2. В названиях переменных рекомендуется использовать PascalCase
 	@ID_Record int
+-- 3. Ключевые слова, названия системных функций и все операторы рекомендуется писать в нижнем регистре
 AS
 set nocount on
 begin
+	-- 4. Рекомендуемое название переменной "@RowsCount"
 	declare @RowCount int = (select count(*) from syn.SA_CustomerSeasonal)
+	-- 5. Рекомендуется при объявления типов не использовать длину поля "max"
 	declare @ErrorMessage varchar(max)
 
 -- Проверка на корректность загрузки
 	if not exists (
+		-- 6. В условных операторах весь блок кода смещается на 1 отступ
 	select 1
+	/*
+		7. Рекомендуется при наименовании алиаса использовать первые заглавные буквы каждого слова в названии объекта,
+		т.е. использовать алиас "imf", т.к. получившийся алиас "if" представляет собой системное слово 
+	*/
 	from syn.ImportFile as f
 	where f.ID = @ID_Record
 		and f.FlagLoaded = cast(1 as bit)
 	)
+	-- 8. Лишний отступ, "begin/end" должны находиться на одном уровне с "if"
 		begin
 			set @ErrorMessage = 'Ошибка при загрузке файла, проверьте корректность данных'
 			raiserror(@ErrorMessage, 3, 1)
+			-- 9. Пустая строка перед "return"
 			return
 		end
 
@@ -30,6 +45,7 @@ begin
 		,cast(isnull(cs.FlagActive, 0) as bit) as FlagActive
 	into #CustomerSeasonal
 	from syn.SA_CustomerSeasonal cs
+		-- 10. Все виды "join"-ов должны указываться явно, пропущено "inner"
 		join dbo.Customer as c on c.UID_DS = cs.UID_DS_Customer
 			and c.ID_mapping_DataSource = 1
 		join dbo.Season as s on s.Name = cs.Season
@@ -45,6 +61,7 @@ begin
 	select
 		cs.*
 		,case
+			-- 11. Результат должен писаться на новой строке с 1 отступом от "when"
 			when c.ID is null then 'UID клиента отсутствует в справочнике "Клиент"'
 			when c_dist.ID is null then 'UID дистрибьютора отсутствует в справочнике "Клиент"'
 			when s.ID is null then 'Сезон отсутствует в справочнике "Сезон"'
@@ -55,8 +72,10 @@ begin
 		end as Reason
 	into #BadInsertedRows
 	from syn.SA_CustomerSeasonal as cs
+		-- 12. Пропущены отступы у "join"-ов
 	left join dbo.Customer as c on c.UID_DS = cs.UID_DS_Customer
 		and c.ID_mapping_DataSource = 1
+		-- 13. Дополнительные условия переносятся на следующую строку с 1 отступом
 	left join dbo.Customer as c_dist on c_dist.UID_DS = cs.UID_DS_CustomerDistributor and c_dist.ID_mapping_DataSource = 1
 	left join dbo.Season as s on s.Name = cs.Season
 	left join syn.CustomerSystemType as cst on cst.Name = cs.CustomerSystemType
@@ -69,6 +88,7 @@ begin
 		or try_cast(isnull(cs.FlagActive, 0) as bit) is null
 
 	-- Обработка данных из файла
+	-- 14. При использовании оператора "merge" ключевое слово "into" не используется
 	merge into syn.CustomerSeasonal as cs
 	using (
 		select
@@ -83,22 +103,27 @@ begin
 	) as s on s.ID_dbo_Customer = cs.ID_dbo_Customer
 		and s.ID_Season = cs.ID_Season
 		and s.DateBegin = cs.DateBegin
+	-- 15. Ключевое слово "then"  записывается на одной строке с  "when", независимо от наличия дополнительных условий
 	when matched
 		and t.ID_CustomerSystemType <> s.ID_CustomerSystemType then
 		update
+		-- 16. Перечисление всех атрибутов с новой строки и одним отступом
 		set ID_CustomerSystemType = s.ID_CustomerSystemType
 			,DateEnd = s.DateEnd
 			,ID_dbo_CustomerDistributor = s.ID_dbo_CustomerDistributor
 			,FlagActive = s.FlagActive
 	when not matched then
+		-- 17. Длинные строки, для повышения читаемости стоит перенести каждый параметр на новую строку с выравниванием стеком
 		insert (ID_dbo_Customer, ID_CustomerSystemType, ID_Season, DateBegin, DateEnd, ID_dbo_CustomerDistributor, FlagActive)
 		values (s.ID_dbo_Customer, s.ID_CustomerSystemType, s.ID_Season, s.DateBegin, s.DateEnd, s.ID_dbo_CustomerDistributor, s.FlagActive);
 
 	-- Информационное сообщение
 	begin
 		select @ErrorMessage = concat('Обработано строк: ', @RowCount)
+		-- 18. Пропущен пробел после объявления оператора "raiserror"
 		raiserror(@ErrorMessage, 1, 1)
 
+		-- 19. Пропущен отступ в комментарии
 		--Формирование таблицы для отчетности
 		select top 100
 			bir.Season as 'Сезон'
@@ -108,6 +133,7 @@ begin
 			,bir.UID_DS_CustomerDistributor as 'UID Дистрибьютора'
 			,bir.CustomerDistributor as 'Дистрибьютор'
 			,isnull(format(try_cast(bir.DateBegin as date), 'dd.MM.yyyy', 'ru-RU'), bir.DateBegin) as 'Дата начала'
+			-- 20. Пропущена точка в "bir.DateEnd"
 			,isnull(format(try_cast(birDateEnd as date), 'dd.MM.yyyy', 'ru-RU'), bir.DateEnd) as 'Дата окончания'
 			,bir.FlagActive as 'Активность'
 			,bir.Reason as 'Причина'
